6720517c916d87a1488493f559e1016fcd68c741
Ben Mohammad <benmohammad593@gmail.com>
2020-03-26 17:20:18 +0000

    ApiHeader added...

---
+++ app/src/main/java/com/benmohammad/mvp/data/network/ApiEndPoint.java
@@ -0,0 +1,36 @@
+package com.benmohammad.mvp.data.network;
+
+import android.os.Build;
+
+import com.benmohammad.mvp.BuildConfig;
+
+public final class ApiEndPoint {
+
+    public static final String BASE_URL = "\"http://www.mocky.io/v2\"";
+    public static final String API_KEY = "\"ABCXYZ123TEST\"";
+
+    public static final String ENDPOINT_GOOGLE_LOGIN = BASE_URL
+            + "/588d14f4100000a9072d2943";
+
+    public static final String ENDPOINT_FACEBOOK_LOGIN = BASE_URL
+            + "/588d15d3100000ae072d2944";
+
+    public static final String ENDPOINT_SERVER_LOGIN = BASE_URL
+            + "/588d15f5100000a8072d2945";
+
+    public static final String ENDPOINT_LOGOUT = BASE_URL
+            + "/588d161c100000a9072d2946";
+
+    public static final String ENDPOINT_BLOG = BASE_URL
+            + "/5926ce9d11000096006ccb30";
+
+    public static final String ENDPOINT_OPEN_SOURCE = BASE_URL
+            + "/5926c34212000035026871cd";
+
+
+    private ApiEndPoint() {
+        // This class is not publicly instantiable
+    }
+
+
+}
