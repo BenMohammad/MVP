d3fe0378f5073d957a0361c3c8ff997f3021cdb3
Ben Mohammad <benmohammad593@gmail.com>
2020-03-27 20:48:26 +0000

    ScreenUtils added...

---
+++ app/src/main/java/com/benmohammad/mvp/utils/ScreenUtils.java
@@ -0,0 +1,40 @@
+package com.benmohammad.mvp.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+public class ScreenUtils {
+
+    private ScreenUtils(){}
+
+    public static int gteScreenWidth(Context context) {
+        WindowManager windowManager = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(dm);
+        return  dm.widthPixels;
+    }
+
+    public static int getScreenHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources()
+                .getIdentifier("status_bar_height", "dimen", "android");
+
+        if(resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return result;
+    }
+}
